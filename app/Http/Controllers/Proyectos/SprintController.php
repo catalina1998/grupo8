df6ccec145e60201f88872e13ce0748a0a3ea801
ivanplanas98 <ivanplanas98@fpuna.edu.py>
2022-10-09 17:50:22 -0300

    deja agregar sprints

--- app/Http/Controllers/Proyectos/SprintController.php
+++ app/Http/Controllers/Proyectos/SprintController.php
@@ -32,12 +32,14 @@ class SprintController extends Controller
         //return $request->descripcion;
         $sprint=new Sprint;
         $date=Carbon::now();
-        
+        //return $request->descripcion;
         $sprint->descripcion=$request->descripcion;
-        $sprint->inicio=Carbon::createFromFormat('Y-m-d',$request->inicio);
-        $sprint->fin=Carbon::createFromFormat('Y-m-d',$request->fin);
-        //return $sprint->inicio->gt($sprint->fin);
-        if($sprint->inicio->gt($sprint->fin) || $sprint->inicio < $date){
+        $sprint->inicio=Carbon::parse($request->inicio);
+        $sprint->fin=Carbon::parse($request->fin);
+        //$fecha_valida= ($sprint->inicio)->gt($sprint->fin);
+        //return $sprint->inicio->lt($date);
+        if($sprint->inicio->gt($sprint->fin) || $sprint->inicio->lt($date))
+        {
             return 'fecha introducida no es valida';
         }
         //return [$sprint->inicio, $sprint->fin];
